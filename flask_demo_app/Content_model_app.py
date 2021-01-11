0beb5039ae1b23292c11a76b79908a7adece9951
nicoivazquez <nv@ufl.edu>
2021-01-11 15:00:54 -0500

    adding html link to table

--- flask_demo_app/Content_model_app.py
+++ flask_demo_app/Content_model_app.py
@@ -77,31 +77,4 @@ def input_to_pred(user_input, vecpkl,modelpkl, review_df): #
         top10_url.append(url)
     return top10_url, item_asin_top10
 
-def input_to_pred_jac(user_input, vecpkl,modelpkl, review_df):
-    user_df = pd.DataFrame({'user1': [user_input]})
-    user_df['cleanText1']= user_df['user1'].apply(lambda words:" ".join([word for word in words.split() if word not in s_words]))
-    user_df['cleanText2'] = user_df['cleanText1'].apply(lambda words:" ".join(["".join([c if c not in punc else " " for c in word]) for word in words.split()]))
-    user_df['cleanText3'] = user_df['cleanText2'].apply(is_only_alpha)
-    user_df['cleanText4'] = user_df['cleanText3'].apply(word_tokenize)
-    user_df['cleanText5'] = user_df['cleanText4'].apply(lambda words: [word.lower() for word in words])
-    user_df['cleanText6'] = user_df['cleanText5'].apply(stemmers)
-    user_df['processed_text'] = user_df['cleanText6'].apply(lambda words: " ".join(words))
-    user_df = user_df.drop(['cleanText1','cleanText2','cleanText3','cleanText4','cleanText5','cleanText6'],axis=1)
-    # Now that the user input went though the text processer I run it though the same vectorizer 
-    user_x = vecpkl.transform([user_df['processed_text'][0]])
-    sim_matrix = jaccard_score(user_x,modelpkl)
-    sim_sorted = np.argsort(sim_matrix)[::-1] # idx of lowest value to highest value but with ::-1 its decending
-    #df['processed_text'][sim_sorted[0][-1]] # looks like it works better going backwards
-    #Geting the text that was most similar to the user input but not needed for flask app url is
-    counter = 1
-    while len(item_asin_top10) < 10:
-        asin = review_df.iloc[sim_sorted[0][-counter]].loc['asin'] # works filtering backwards
-        counter += 1
-        if asin not in item_asin_top10:
-            item_asin_top10.append(str(asin))
-        else: 
-            continue
-    for asin in item_asin_top10:
-        url = "http://www.amazon.com/dp/"+asin
-        top10_url.append(url)
-    return top10_url, item_asin_top10
+
